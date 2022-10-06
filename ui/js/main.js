97d3cb909b73388acd3777e9babaa933a36857fb
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-10-06 10:09:08 +0530

    Update main.js

--- ui/js/main.js
+++ ui/js/main.js
@@ -7,6 +7,36 @@ console.log("started");
 	var app = angular.module('cetApp', []);
 	app.controller('cetCtrl', function($scope,$http,$window, $location, $anchorScroll, $timeout,$filter) {
 		
+		
+		
+		$scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+		
+		
+		
+		
 		$scope.color = {
     'background-color': 'green'
   };
