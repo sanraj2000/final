a5cc611dda8215580937fb9dcc7b66e7dea0cb2c
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-10-10 12:27:55 +0530

    Update main.js

--- ui/js/main.js
+++ ui/js/main.js
@@ -15,15 +15,23 @@ var test1 = {
    }
 $http(test1).then(function(response2) {
     $scope.aw = response2.data;
-    const t =  $scope.aw
+    var t =  $scope.aw
     var a = "N/A"
     if(t){
 	   const myArray = t.split(" ");
+	    if(myArray[0]){
     $scope.test1total = myArray[0];
     $scope.test1fail = myArray[3];
    // $scope.test1skip = myArray[5];
 	    $scope.test1skip = "Hello";
-    $scope.test1pass = Number(myArray[0])-Number(myArray[3])-Number(myArray[5]); 
+    $scope.test1pass = Number(myArray[0])-Number(myArray[3])-Number(myArray[5]); }
+	    else{
+		     $scope.check = "testing";
+	    $scope.test1total = "Not";
+    $scope.test1fail = "N/A";
+    $scope.test1skip = "N/A";
+    $scope.test1pass = a;
+	    }
 	    
 	    
 	     
